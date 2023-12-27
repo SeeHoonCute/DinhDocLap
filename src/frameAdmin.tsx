eee273bfaf25c5f9968c3214cec4f2c18fb106a2
varia <ltnhan2610@gmail.com>
2023-12-27 17:19:00 +0700

    install recoil and uncomment handleLogin

--- src/frameAdmin.tsx
+++ src/frameAdmin.tsx
@@ -6,19 +6,17 @@ import { Search } from "./components/Search/Search";
 import { useNavigate } from "react-router-dom";
 const FrameAdmin = () => {
 
-    // console.log('localStorage: ' + localStorage.getItem('admin'));
-    // console.log('Lenght: ' + localStorage.length);
     let navigate = useNavigate();
-    // const handleLogin = () => {
-    //     let path = `/login`;
-    //     navigate(path);
-    // }
-    //
-    // useEffect(() => {
-    //     if(!localStorage.getItem('admin')){
-    //         return handleLogin();
-    //     }
-    // }, []);
+    const handleLogin = () => {
+        let path = `/login`;
+        navigate(path);
+    }
+    
+    useEffect(() => {
+        if(!localStorage.getItem('admin')){
+            return handleLogin();
+        }
+    }, []);
     return (
         <>
             <AppShell>
